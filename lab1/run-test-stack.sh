9bd71b1f7dee3633dfdfe1af12c8fb26cc03e8bc
ksedixxkp030 <76788207+ksedix@users.noreply.github.com>
2024-01-13 17:34:55 +0100

    Add files via upload
    Assignment 1

---
+++ lab1/run-test-stack.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Programming Language Technology (Chalmers DAT151 / GU DIT231)
+# (C) 2022-23 Andreas Abel
+# All rights reserved.
+
+# Run PLT lab 1 testsuite on grammar file CC.cf
+
+root="$PWD"
+cd testsuite
+stack run -- "$root/CC.cf"
